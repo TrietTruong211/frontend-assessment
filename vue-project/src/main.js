ce624daa999b56db533d3c2af1fdb198571894d9
TrietTruongMinh <triet.truongminh211@gmail.com>
2022-04-11 14:07:09 +1000

    Removing vue 3 project

--- vue-project/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import {Tabs, Tab} from 'vue3-tabs-component';
-
-createApp(App).component('tabs', Tabs)
-.component('tab', Tab).mount('#app') 
